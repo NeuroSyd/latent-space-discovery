ca3cb9c6df6fda06ff1493555c7ab8174f8efcaf
Raktim Kumar Mondol <raktim.live@gmail.com>
2021-02-03 19:03:25 +0600

    Update variational_autoencoder.py

--- variational_autoencoder.py
+++ variational_autoencoder.py
@@ -87,7 +87,7 @@ def vae_model_single(path,original_dim,xtrain,xtest,intermediate_dim,batch_size,
     vae.compile(optimizer='adadelta', loss=None)
     
     vae.summary()
-    history=vae.fit(xtrain, None, epochs=epochs, batch_size=batch_size, validation_data=(xtest, None))
+    history=vae.fit(xtrain, None, epochs=epochs, batch_size=batch_size)
     df = pd.DataFrame(history.history)
     df.to_csv(os.path.join(path, "vae_training_history.csv"))
     encoder.save(os.path.join(path,"vae_encoder.h5"))
